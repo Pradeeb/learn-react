7343cb63d1e91d590c54e6b54f214d6c0df5d9f2
pradeeb <valanpradeep6@gmail.com>
2024-10-16 17:20:43 +0530

    usestate learned next use effect

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import LearnUsestate from './learning hooks/learn usestate hook/LearnUsestate'
+import LearnUseeffect from './learning hooks/learn useeffect hook/LearnUseeffect'
 
 function App() {
   return (
     <div className="App">
     <LearnUsestate/>
+    <LearnUseeffect/>
     </div>
   );
 } 
