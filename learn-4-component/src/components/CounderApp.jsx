24e961b53ed9e9f1246402b392208efb14b98cb8
pradeeb <valanpradeep6@gmail.com>
2025-04-13 15:38:47 +0530

    feat: conditional formateing

--- learn-4-component/src/components/CounderApp.jsx
+++ learn-4-component/src/components/CounderApp.jsx
@@ -15,11 +15,36 @@ function CounderApp() {
         
     }
 
+    let message;
+    if(count>= 10&& count<=20){
+        message=(
+        <div>
+            <h3>you clicked{count} time</h3>
+        </div>
+        )
+    }else if(count< 10){
+        message=(
+            <div>
+                <h3>stil dont reach the minimum rewarts</h3>
+            </div>
+            )
+    }else{
+        message =(
+            <div>
+            <h3>You are master</h3>
+        </div>
+        )
+            
+        }
+    
+
   return (
     <div>
         <h1>Counter count -{count} </h1>
         <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 my-1 mx-1 rounded" onClick={increase}>Increase</button>
         <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 my-1 rounded" onClick={decrease}>Decrease</button>
+        {count >= 10 ? <p>you unlocked a 10% Discount</p> : <p>click 10 timed to unlock Reward</p>}
+        {message}
     </div>
   )
 }
