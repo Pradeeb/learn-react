689b346f81c8a1d00964186f6259f2409a34de65
pradeeb <valanpradeep6@gmail.com>
2025-04-16 14:56:31 +0530

    feat ; learn usesate hook

--- learn-4-component/src/App.jsx
+++ learn-4-component/src/App.jsx
@@ -2,6 +2,7 @@ import Footer from './components/Footer';
 import Header from './components/Header';
 import Content from './components/Content'
 import { createContext, useState } from 'react';
+import UseStateLearn from './components/UseStateLearn';
 
 export let userContext=createContext();                        {/*Use context api use state pass*/}
 
@@ -20,7 +21,8 @@ function App() {
       <userContext.Provider value={{user}}>                      {/*Use context api use state pass*/}
       <div className='w-1/2 mx-auto border-2'>
         <Header user={user} />                                   {/*user={user} this is props */} 
-        <Content user={user} />
+        {/* <Content user={user} /> */}
+        <UseStateLearn/>
         <Footer user={user} />
       </div>
       </userContext.Provider>
