3807d6eefc117b8566430655deacf611f9d37bf8
Pradeeb <valanpradeep6@gmail.com>
2025-11-24 17:04:42 +0530

    command added

--- learn-hook/src/Components/UseCallback/Product.jsx
+++ learn-hook/src/Components/UseCallback/Product.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 
+// React.memo => use for avoid to re render if without modified props value come
 const Product = React.memo(({product ,addToCard}) => {
     console.log("Product page rendered");
     
   return (
-    <div>
+    <div style={{border:"2px solid black",  width:"50%" , margin:"10px" , padding:"10px"}}>
         <h1>Product name : {product}</h1>
         <button onClick={addToCard}>add to cart</button>
     </div>
